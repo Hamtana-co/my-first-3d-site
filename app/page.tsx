2ab2b55944d606de62d8c36df30b829fc924ffa9
Hamtana <hamtana.acc@gmail.com>
2023-08-28 16:39:17 +0330

    init playcanvas build

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+/** @format */
+
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <>
+      <script src='./PlayCanvas/__modules__.js'></script>
+      <script src='./PlayCanvas/__start__.js'></script>
+      <script src='./PlayCanvas/__loading__.js'></script>
+    </>
+  )
+}
---
+++ public/PlayCanvas/__settings__.js
@@ -0,0 +1,44 @@
+/** @format */
+
+window.ASSET_PREFIX = ''
+window.SCRIPT_PREFIX = ''
+window.SCENE_PATH = './PlayCanvas/1843149.json'
+window.CONTEXT_OPTIONS = {
+  antialias: true,
+  alpha: false,
+  preserveDrawingBuffer: false,
+  preferWebGl2: false,
+  powerPreference: 'default',
+}
+window.SCRIPTS = [147239088, 147239093, 147239095, 147239096, 147239097, 147239099, 147239108]
+window.CONFIG_FILENAME = './PlayCanvas/config.json'
+window.INPUT_SETTINGS = {
+  useKeyboard: true,
+  useMouse: true,
+  useGamepads: false,
+  useTouch: true,
+}
+pc.script.legacy = false
+window.PRELOAD_MODULES = [
+  {
+    moduleName: 'Ammo',
+    glueUrl: './PlayCanvas/files/assets/147239106/1/ammo.wasm.js',
+    wasmUrl: './PlayCanvas/files/assets/147239107/1/ammo.wasm.wasm',
+    fallbackUrl: './PlayCanvas/files/assets/147239105/1/ammo.js',
+    preload: true,
+  },
+  {
+    moduleName: 'BASIS',
+    glueUrl: './PlayCanvas/files/assets/147239076/1/basis.wasm.js',
+    wasmUrl: './PlayCanvas/files/assets/147239077/1/basis.wasm.wasm',
+    fallbackUrl: './PlayCanvas/files/assets/147239075/1/basis.js',
+    preload: false,
+  },
+  {
+    moduleName: 'DracoDecoderModule',
+    glueUrl: './PlayCanvas/files/assets/147239416/1/draco.wasm.js',
+    wasmUrl: './PlayCanvas/files/assets/147239417/1/draco.wasm.wasm',
+    fallbackUrl: './PlayCanvas/files/assets/147239415/1/draco.js',
+    preload: true,
+  },
+]
